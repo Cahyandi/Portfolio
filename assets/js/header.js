9e2c0fccfc93be3d2f2bd3b7e4b6423d30779c2e
yndi24 <yandicimahi123@gmail.com>
2022-04-11 23:26:14 +0700

    Header

---
+++ assets/js/header.js
@@ -0,0 +1,8 @@
+function tabChange() {
+    var tabs = ('.header-link > li');
+    var active = tabs.filter('.active');
+    var next = active.next('li').length? active.next('li').find('a') : tabs.filter(':first-child').find('a');
+    next.tab('show');
+}
+
+tabChange();
