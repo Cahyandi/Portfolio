d52529de2f009511c2e321e4ad29b1e22f8b518d
yndi24 <yandicimahi123@gmail.com>
2022-04-15 21:12:22 +0700

    hamburger-menu

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,8 +1,73 @@
-function tabChange() {
-    var tabs = ('.header-link > li');
-    var active = tabs.filter('.active');
-    var next = active.next('li').length? active.next('li').find('a') : tabs.filter(':first-child').find('a');
-    next.tab('show');
-}
+const sections = document.querySelectorAll('section');
+const navlia = document.querySelectorAll('nav .navigation ul li');
 
-tabChange();
+
+    window.addEventListener('scroll', ()=> {
+        let current = '';
+
+        sections.forEach( section =>{
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if (pageYOffset >= sectionTop - sectionHeight / 3) {
+            current = section.getAttribute('id');
+            }
+        })
+        
+        navlia.forEach( li => {
+            li.classList.remove('aktif');
+            if (li.classList.contains(current)) {
+                li.classList.add('aktif')
+            }
+            })
+    })  
+
+
+
+
+    // Hamburger menu
+    const hamburgerMenu = document.querySelector("#hamburger-menu");
+    const overlay = document.querySelector("#overlay");
+    const nav1 = document.querySelector("#nav-1");
+    const nav2 = document.querySelector("#nav-2");
+    const nav3 = document.querySelector("#nav-3");
+    const nav4 = document.querySelector("#nav-4");
+    const nav5 = document.querySelector("#nav-5");
+    const nav6 = document.querySelector("#nav-6");
+    const navItems = [nav1, nav2, nav3, nav4, nav5, nav6];
+
+    
+// Control Navigation Animation
+function navAnimation(val1, val2) {
+    navItems.forEach((nav, i) => {
+      nav.classList.replace(`slide-${val1}-${i + 1}`, `slide-${val2}-${i + 1}`);
+    });
+  }
+  
+  function toggleNav() {
+    // Toggle: Hamburger Open/Close
+    hamburgerMenu.classList.toggle("active");
+  
+    //   Toggle: Menu Active
+    overlay.classList.toggle("overlay-active");
+  
+    if (overlay.classList.contains("overlay-active")) {
+      // Animate In - Overlay
+      overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
+  
+      // Animate In - Nav Items
+      navAnimation("out", "in");
+    } else {
+      // Animate Out - Overlay
+      overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
+  
+      // Animate Out - Nav Items
+      navAnimation("in", "out");
+    }
+  }
+  
+  // Events Listeners
+  hamburgerMenu.addEventListener("click", toggleNav);
+  navItems.forEach((nav) => {
+    nav.addEventListener("click", toggleNav);
+  });
+  
