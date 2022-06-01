82d105ffef7d5739835069cfa987716968cf4b3c
yndi24 <yandicimahi123@gmail.com>
2022-06-01 10:22:07 +0700

    :

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,5 +1,5 @@
 const sections = document.querySelectorAll('section');
-const navlia = document.querySelectorAll('nav .navigation ul li');
+const navlia = document.querySelectorAll('nav .container ul li');
 
 
     window.addEventListener('scroll', ()=> {
@@ -14,49 +14,59 @@ const navlia = document.querySelectorAll('nav .navigation ul li');
         })
         
         navlia.forEach( li => {
-            li.classList.remove('aktif');
+            li.classList.remove('active');
             if (li.classList.contains(current)) {
-                li.classList.add('aktif')
+                li.classList.add('active');
             }
             })
     })  
 
 
-
-
-    // Hamburger menu
-    const hamburgerMenu = document.querySelector("#hamburger-menu");
-    const overlay = document.querySelector("#overlay");
     
+//     // Hamburger menu
+//     const hamburgerMenu = document.querySelector("#hamburger-menu");
+//     const overlay = document.querySelector("#overlay");
+//     const nav1 = document.querySelector("#nav-1");
+//     const nav2 = document.querySelector("#nav-2");
+//     const nav3 = document.querySelector("#nav-3");
+//     const nav4 = document.querySelector("#nav-4");
+//     const nav5 = document.querySelector("#nav-5");
+//     const nav6 = document.querySelector("#nav-6");
+//     const navItems = [nav1, nav2, nav3, nav4, nav5, nav6];
 
     
-
+// // Control Navigation Animation
+// function navAnimation(val1, val2) {
+//     navItems.forEach((nav, i) => {
+//       nav.classList.replace(`slide-${val1}-${i + 1}`, `slide-${val2}-${i + 1}`);
+//     });
+//   }
   
-  function toggleNav() {
-    // Toggle: Hamburger Open/Close
-    hamburgerMenu.classList.toggle("active");
+//   function toggleNav() {
+//     // Toggle: Hamburger Open/Close
+//     hamburgerMenu.classList.toggle("active");
   
-    //   Toggle: Menu Active
-    overlay.classList.toggle("overlay-active");
+//     //   Toggle: Menu Active
+//     overlay.classList.toggle("overlay-active");
   
-    if (overlay.classList.contains("overlay-active")) {
-      // Animate In - Overlay
-      overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
+//     if (overlay.classList.contains("overlay-active")) {
+//       // Animate In - Overlay
+//       overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
   
-      // Animate In - Nav Items
-      navAnimation("out", "in");
-    } else {
-      // Animate Out - Overlay
-      overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
+//       // Animate In - Nav Items
+//       navAnimation("out", "in");
+//     } else {
+//       // Animate Out - Overlay
+//       overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
   
-      // Animate Out - Nav Items
-      navAnimation("in", "out");
-    }
-  }
+//       // Animate Out - Nav Items
+//       navAnimation("in", "out");
+//     }
+//   }
   
-  // Events Listeners
-  hamburgerMenu.addEventListener("click", toggleNav);
-  navItems.forEach((nav) => {
-    nav.addEventListener("click", toggleNav);
-  });
+//   // Events Listeners
+//   hamburgerMenu.addEventListener("click", toggleNav);
+//   navItems.forEach((nav) => {
+//     nav.addEventListener("click", toggleNav);
+//   });
   
