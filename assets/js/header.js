c35f27e8f7683dc21662834e3e9c1ce2c6038d27
yndi24 <yandicimahi123@gmail.com>
2022-06-18 09:23:00 +0700

    Header and Navbar

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,5 +1,5 @@
 const sections = document.querySelectorAll('section');
-const navlia = document.querySelectorAll('nav .container ul li');
+const navlia = document.querySelectorAll('section .container ul li');
 
 
     window.addEventListener('scroll', ()=> {
@@ -21,52 +21,17 @@ const navlia = document.querySelectorAll('nav .container ul li');
             })
     })  
 
+// Hamburger Menu 
+let navbar = document.querySelector('.navbar');
+let hamburgerMenu = document.querySelector("#hamburger");
 
-    
-//     // Hamburger menu
-//     const hamburgerMenu = document.querySelector("#hamburger-menu");
-//     const overlay = document.querySelector("#overlay");
-//     const nav1 = document.querySelector("#nav-1");
-//     const nav2 = document.querySelector("#nav-2");
-//     const nav3 = document.querySelector("#nav-3");
-//     const nav4 = document.querySelector("#nav-4");
-//     const nav5 = document.querySelector("#nav-5");
-//     const nav6 = document.querySelector("#nav-6");
-//     const navItems = [nav1, nav2, nav3, nav4, nav5, nav6];
+hamburgerMenu.onclick = () => {
+    navbar.classList.toggle('active');
+    hamburgerMenu.classList.toggle('active');
+}
 
-    
-// // Control Navigation Animation
-// function navAnimation(val1, val2) {
-//     navItems.forEach((nav, i) => {
-//       nav.classList.replace(`slide-${val1}-${i + 1}`, `slide-${val2}-${i + 1}`);
-//     });
-//   }
-  
-//   function toggleNav() {
-//     // Toggle: Hamburger Open/Close
-//     hamburgerMenu.classList.toggle("active");
-  
-//     //   Toggle: Menu Active
-//     overlay.classList.toggle("overlay-active");
-  
-//     if (overlay.classList.contains("overlay-active")) {
-//       // Animate In - Overlay
-//       overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
-  
-//       // Animate In - Nav Items
-//       navAnimation("out", "in");
-//     } else {
-//       // Animate Out - Overlay
-//       overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
-  
-//       // Animate Out - Nav Items
-//       navAnimation("in", "out");
-//     }
-//   }
-  
-//   // Events Listeners
-//   hamburgerMenu.addEventListener("click", toggleNav);
-//   navItems.forEach((nav) => {
-//     nav.addEventListener("click", toggleNav);
-//   });
-  
+// Menutup Class avtive jika user telah mengklik salah satu navbar
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    hamburgerMenu.classList.remove('active');
+}
